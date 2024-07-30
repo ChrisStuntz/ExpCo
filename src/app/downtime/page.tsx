af89b33b470c2a70beb666af00f87aa7dce3a0d7
Chris <cstuntz@fastmail.com>
2024-07-29 22:16:21 -0700

    Add in top nav and new icon

---
+++ src/app/downtime/page.tsx
@@ -0,0 +1,9 @@
+export default function DowntimePage() {
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-center">
+            <div className="container flex flex-col items-center justify-center gap-12 px-4 py-4">
+                Downtime goes here
+            </div>
+        </main>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -40,7 +40,7 @@ const mockTasks = [
 
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
+    <main className="flex min-h-screen flex-col items-center justify-center">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-4">
         <div className="flex">
         <Table>
