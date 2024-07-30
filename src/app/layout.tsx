dd3c745aa5047afb987a2668ec51209a43e663ab
Chris <cstuntz@fastmail.com>
2024-07-29 22:24:14 -0700

    Fix build

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
-export function TopNav() {
+function TopNav() {
   return (
     <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
       <div>Downtime</div>
