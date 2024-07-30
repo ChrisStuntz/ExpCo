a3a521f584e99e06e4a0c151ad773c74b90c430f
Chris <cstuntz@fastmail.com>
2024-07-29 22:52:54 -0700

    Convert to data table, add mock dropdown menu actions

---
+++ src/app/downtime/columns.tsx
@@ -0,0 +1,75 @@
+"use client"
+
+import { MoreHorizontal } from "lucide-react"
+
+import { ColumnDef } from "@tanstack/react-table"
+import { Button } from "~/components/ui/button"
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "~/components/ui/dropdown-menu"
+
+
+export type Task = {
+    id: number
+    name: string
+    skills: string
+    dc: number
+    total_points: number
+    points_remaining: number
+}
+
+export const columns: ColumnDef<Task>[] = [
+    {
+        accessorKey: "name",
+        header: "Name",
+    },
+    {
+        accessorKey: "skills",
+        header: "Skills",
+    },
+    {
+        accessorKey: "dc",
+        header: "Task DC",
+    },
+    {
+        accessorKey: "total_points",
+        header: "Total Points",
+    },
+    {
+        accessorKey: "points_remaining",
+        header: "Points Remaining",
+    },
+    {
+        id: "actions",
+        cell: ({ row }) => {
+          const task = row.original
+     
+          return (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" className="h-8 w-8 p-0">
+                  <span className="sr-only">Open menu</span>
+                  <MoreHorizontal className="h-4 w-4" />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>Actions</DropdownMenuLabel>
+                <DropdownMenuItem
+                  onClick={() => navigator.clipboard.writeText(task.name)}
+                >
+                  Copy Task Name
+                </DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Edit</DropdownMenuItem>
+                <DropdownMenuItem>Delete</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )
+        },
+      },
+]
--- src/app/downtime/page.tsx
+++ src/app/downtime/page.tsx
@@ -8,64 +8,46 @@ import {
     TableRow,
   } from "~/components/ui/table"
   
-  
-  const mockTasks = [
-    {
-      id: 1,
-      name: "Build Quarry",
-      skills: "Athletics, Crafting",
-      dc: 15,
-      total_points: 25,
-      points_remaining: 8,
-    },
-    {
-      id: 2,
-      name: "Study Notes",
-      skills: "Arcana, Occultism",
-      dc: 20,
-      total_points: 40,
-      points_remaining: 34,
-    },
-    {
-      id: 3,
-      name: "Razdius Manual",
-      skills: "Religion, Acamaya Lore",
-      dc: 22,
-      total_points: 60,
-      points_remaining: 23
-    },
-  ];
+  import { Task, columns } from "./columns"
+  import { DataTable } from "./data-table"
 
-export default function DowntimePage() {
+  async function getData(): Promise<Task[]> {
+    return [
+        {
+          id: 1,
+          name: "Build Quarry",
+          skills: "Athletics, Crafting",
+          dc: 15,
+          total_points: 25,
+          points_remaining: 8,
+        },
+        {
+          id: 2,
+          name: "Study Notes",
+          skills: "Arcana, Occultism",
+          dc: 20,
+          total_points: 40,
+          points_remaining: 34,
+        },
+        {
+          id: 3,
+          name: "Razdius Manual",
+          skills: "Religion, Acamaya Lore",
+          dc: 22,
+          total_points: 60,
+          points_remaining: 23
+        },
+      ]
+}
+
+export default async function DowntimePage() {
+    const data = await getData()
+
     return (
         <main className="flex min-h-screen flex-col items-center justify-center">
             <div className="container flex flex-col items-center justify-center gap-12 px-4 py-4">
-                <div className="flex">
-                <Table>
-                    <TableCaption>All the downtime activities</TableCaption>
-                    <TableHeader>
-                        <TableRow>
-                        <TableHead className="text-center">Name</TableHead>
-                        <TableHead className="text-center">Skills</TableHead>
-                        <TableHead className="text-center">DC</TableHead>
-                        <TableHead className="text-center">Total Points</TableHead>
-                        <TableHead className="text-center">Points Remaining</TableHead>
-                        </TableRow>
-                    </TableHeader>
-                    <TableBody>
-                        {
-                        mockTasks.map((task) => (
-                            <TableRow key={task.id}>
-                            <TableCell className="text-center">{task.name}</TableCell>
-                            <TableCell className="text-center">{task.skills}</TableCell>
-                            <TableCell className="text-center">{task.dc}</TableCell>
-                            <TableCell className="text-center">{task.total_points}</TableCell>
-                            <TableCell className="text-center">{task.points_remaining}</TableCell>
-                            </TableRow>
-                        ))
-                        }
-                    </TableBody>
-                </Table>
+                <div className="container mx-auto py-10">
+                    <DataTable columns={columns} data={data} />
                 </div>
             </div>
         </main>
